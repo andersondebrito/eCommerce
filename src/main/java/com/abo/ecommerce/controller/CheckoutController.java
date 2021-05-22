bc5a21df7160401ddd40127b6e817a6259d30466
andersondebrito <andersondebritto@gmail.com>
2021-05-22 22:53:58 +0100

    RestController

---
+++ src/main/java/com/abo/ecommerce/controller/CheckoutController.java
@@ -0,0 +1,26 @@
+package com.abo.ecommerce.controller;
+
+import com.abo.ecommerce.dto.Purchase;
+import com.abo.ecommerce.dto.PurchaseResponse;
+import com.abo.ecommerce.service.CheckoutService;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:4200")
+@RestController
+@RequestMapping("/api/checkout")
+public class CheckoutController {
+
+    private CheckoutService checkoutService;
+
+    public CheckoutController(CheckoutService checkoutService) {
+        this.checkoutService = checkoutService;
+    }
+
+    @PostMapping("/purchase")
+    public PurchaseResponse placeOrder(@RequestBody Purchase purchase){
+
+        PurchaseResponse purchaseResponse = checkoutService.placeOrder(purchase);
+
+        return purchaseResponse;
+    }
+}
