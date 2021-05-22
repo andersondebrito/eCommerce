5c5f8938919173875cdb0a10a0f173f41b27e118
andersondebrito <andersondebritto@gmail.com>
2021-05-22 22:43:57 +0100

    Repository + Service

--- src/main/java/com/abo/ecommerce/service/CheckoutServiceImpl.java
+++ src/main/java/com/abo/ecommerce/service/CheckoutServiceImpl.java
@@ -2,8 +2,15 @@ package com.abo.ecommerce.service;
 
 import com.abo.ecommerce.dto.Purchase;
 import com.abo.ecommerce.dto.PurchaseResponse;
+import com.abo.ecommerce.entity.Customer;
+import com.abo.ecommerce.entity.Order;
+import com.abo.ecommerce.entity.OrderItem;
 import com.abo.ecommerce.repository.CustomerRepository;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.Set;
+import java.util.UUID;
 
 @Service
 public class CheckoutServiceImpl implements CheckoutService{
@@ -15,7 +22,39 @@ public class CheckoutServiceImpl implements CheckoutService{
     }
 
     @Override
+    @Transactional
     public PurchaseResponse placeOrder(Purchase purchase) {
-        return null;
+
+        // retrieve the order  info from dto
+        Order order = purchase.getOrder();
+
+        // generate tracking number
+        String orderTrackingNumber = generateOrderTrackingNumber();
+        order.setOrderTrackingNumber(orderTrackingNumber);
+
+        // populate order with orderItems
+        Set<OrderItem> orderItems = purchase.getOrderItems();
+        orderItems.forEach(item -> order.add(item));
+
+        // populate order with billingAddress and shippingAddress
+        order.setBillingAddress(purchase.getBillingAddress());
+        order.setShippingAddress(purchase.getShippingAddress());
+
+        // populate customer with order
+        Customer customer = purchase.getCustomer();
+        customer.add(order);
+
+        // save to the database
+        customerRepository.save(customer);
+
+        // return a response
+        return new PurchaseResponse(orderTrackingNumber);
+    }
+
+    private String generateOrderTrackingNumber() {
+
+        // generate a random UUID number (UUID version-4)
+        // For details see: Https://en.wikipedia.org/wiki/Universally_unique_identifier
+        return UUID.randomUUID().toString();
     }
 }
