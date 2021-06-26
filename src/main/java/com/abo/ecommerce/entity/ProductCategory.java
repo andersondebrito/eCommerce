c8d0f02b483ff535ddac9dc8ffd74049692e31ea
andersondebrito <andersondebritto@gmail.com>
2021-06-26 20:18:25 +0100

    Secure Backend

--- src/main/java/com/abo/ecommerce/entity/ProductCategory.java
+++ src/main/java/com/abo/ecommerce/entity/ProductCategory.java
@@ -1,14 +1,15 @@
 package com.abo.ecommerce.entity;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
-import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 import java.util.Set;
 
 @Entity
 @Table(name = "product_category")
-@Data
+@Getter
+@Setter
 public class ProductCategory {
 
     @Id
